1b67b59346e1c845ad55f48560b4b715e495688f
Abhimonyu Dey <adey.edunet@gmail.com>
2021-07-13 14:25:20 +0530

    first commit

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var e=a();
 
 e.get("/",function(req,res)
 			{
-				res.send("Updated Welcome to Node");
+				res.send("Any Updated Welcome to Node");
 			}
 	);
-var p=process.env.port || 3000;
+var p=process.env.port || 5000;
 e.listen(p);
